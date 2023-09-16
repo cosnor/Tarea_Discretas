7c99fb679a9a823859326040989c6cd88871ae6a
Loreann Valencia Pantoja <loreannv@uninorte.edu.co>
2023-09-15 23:31:55 -0500

    final

--- main.py
+++ main.py
@@ -9,6 +9,36 @@ import tkinter as tk
 import numpy as np
 import networkx as nx
 
+import sys; print(sys.executable)
+class Grafo: 
+    def __init__(self):
+        self.nodos = []
+        self.aristas = []
+        self.mostrar = []
+        self.texto= ""
+        
+    def teorema(self, grados: list) -> bool:
+        #Ordena la lista de mayor a menor
+        lista_ord = sorted(grados, reverse=True)
+        self.mostrar.append(lista_ord.copy())
+        #Sale si se queda sin lista, o si hay un -1, o si todos son 0
+        while len(lista_ord) > 0 and lista_ord.count(-1) == 0 and lista_ord.count(0) != len(lista_ord):
+            print(lista_ord)
+            to_remove = lista_ord.pop(0)
+            for i in range(to_remove):
+                lista_ord[i] -= 1
+                
+            lista_ord.sort(reverse=True)
+            self.mostrar.append(lista_ord.copy())
+            print(lista_ord)
+        print(lista_ord)
+        
+        if lista_ord.count(-1) > 0:
+            self.texto = "El grafo simple no existe"
+            return False
+        else:
+            self.texto = "El grafo simple sí existe"
+            return True
 
 class App(customtkinter.CTk):
     def __init__(self):
